d927b65f150f2ef753393b22fb2acfee89131f0f
Work <Work@HOME-PC>
2023-11-10 01:10:54 +0400

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Accord.Video.DirectShow;
+using CameraTemp.Webcams;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static CameraTemp.Webcams.WebcamsLogic;
+
+namespace CameraTemp
+{
+    public partial class MainWindow : Window
+    {
+        private readonly WebcamsLogic WebcamsLogic = new WebcamsLogic();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            WebcamsLogic.FillWebcams(Combo);
+            WebcamsLogic.SetFirstWebcam(Combo);
+            WebcamsLogic._MainWindow = this;
+        }
+
+        private void Combo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            WebcamsLogic.StopWebcam();
+            WebcamsLogic.StartWebcam(Combo);
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            WebcamsLogic.StopWebcam();
+        }
+
+    }
+}
---
+++ Webcams/WebcamsLogic.cs
@@ -0,0 +1,80 @@
+﻿using Accord.Video;
+using Accord.Video.DirectShow;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Security.Permissions;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace CameraTemp.Webcams
+{
+    public class WebcamsLogic
+    {
+        public MainWindow _MainWindow;
+
+        private FilterInfoCollection _availableWebcams;
+
+        private VideoCaptureDevice _webcam;
+
+        public void FillWebcams(ComboBox comboBox)
+        {
+            _availableWebcams = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            foreach (var webcam in _availableWebcams)
+            {
+                comboBox.Items.Add(webcam.Name);
+            }
+        }
+
+        public void SetFirstWebcam (ComboBox comboBox)
+        {
+            if (comboBox.Items.Count > 0)
+            {
+                comboBox.SelectedIndex = 0;
+            }
+        }
+
+        public void StartWebcam(ComboBox comboBox)
+        {
+            _availableWebcams = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            _webcam = new VideoCaptureDevice(_availableWebcams[comboBox.SelectedIndex].MonikerString);
+            _webcam.Start();
+            _webcam.NewFrame += new NewFrameEventHandler(WebcamNewFrame);
+        }
+
+        public void StopWebcam()
+        {
+            if (_webcam != null && _webcam.IsRunning == true)
+            {
+                _webcam.SignalToStop();
+                _webcam.NewFrame -= new NewFrameEventHandler(WebcamNewFrame);
+                _webcam = null;
+            }
+        }
+
+        public void WebcamNewFrame (object sender, NewFrameEventArgs newFrame)
+        {
+            _MainWindow.Dispatcher.Invoke(() =>
+            {
+                _MainWindow.Img.Source = BitmapToBitImage(newFrame.Frame);
+            });
+        }
+
+        public BitmapImage BitmapToBitImage(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, ImageFormat.Bmp);
+                memory.Position = 0;
+                System.Windows.Media.Imaging.BitmapImage bitmapImage = new System.Windows.Media.Imaging.BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                return bitmapImage;
+            }
+        }
+    }
+}
